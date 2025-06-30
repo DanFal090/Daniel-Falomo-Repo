264715d4bf80e740d6890b4d15cfaea2e0ed400e
DanFal090 <daniel.falomo@yahoo.com>
2025-06-30 12:37:28 +0100

    Python code

---
+++ hello.py
@@ -0,0 +1,67 @@
+# variable = container of value. Behaves as the value
+
+#first_name = "Joy"
+#last_name = "Falomo"
+#full_name = first_name + " " + last_name
+#print("Hello "+full_name)
+#print(type(full_name))
+
+#age = 30
+#age = age + 1
+#print("Joy's age is: "+str(age))
+#print(type(age))
+
+#height = 250.5
+#print("Joy's height is: "+str(height)+"cm")
+#print(type(height))
+
+#human = True
+#print("Is Joy a human: "+str(human))
+#print(type(human))
+
+
+# Multiple assignment = allows us to assign multiple variables at the same time in one line of code
+
+#Joy = 30
+#Daniel = 30
+#Faith = 30
+
+#Joy = Daniel = Faith = 30
+#print(Joy)
+#print(Daniel)
+#print(Faith)
+
+#name = "Joy is "+str(30) + "years old and she is "+str(28.8)+"cm tall." + " Daniel is going to be "+str(35) +"years old and he is "+str(205.8)+"cm tall."
+#print(len(name))
+
+#name = "joy"
+#print(name.find("y"))
+#print(name.capitalize())
+#print(name.upper())
+#print(name.isdigit())
+#print(name.isalpha())
+#print(name.replace("o","a"))
+#print(name*3)
+
+#Type Casting = convert the data type of a value to another type.
+
+#x = 1
+#y = 2.0
+#z = "3"
+#x = float(x)
+#y = int(y)
+#z = int(z)
+#print(name, x, y, z)
+#print(x+0.1)
+#print(y)
+#print(z*3)
+
+name = input("What is your name?: ")
+age = input("How old are you?: ")
+
+age = int(age) + 1
+print("Hello, " + name)
+print("Your age is " + str(age)+" years old")
+
+
+
